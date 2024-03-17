ea52b474f638f67daf8e084de3f734aad86b6067
Ashish <ashish74624@gmail.com>
2024-03-17 16:56:42 +0530

    myenv added

--- client/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Navbar() {
-  return (
-    <nav className='bg-green-400 w-screen py-5 px-4'>
-      <h1 className='text-2xl text-white'>Plagiarism Checker</h1>
-    </nav>
-  )
-}
